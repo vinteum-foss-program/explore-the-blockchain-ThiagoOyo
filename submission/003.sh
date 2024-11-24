88b1cf564940930c479b997a2c4565ad437e8f6b
thgOyo <thiagooyo@gmail.com>
2024-11-24 11:42:35 -0300

    003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,23 @@
 # How many new outputs were created by block 123,456?
+
+total_outputs=0
+
+block=123456
+
+block_info=$(bitcoin-cli getblock $(bitcoin-cli getblockhash $block))
+
+block_txs=$(echo "$block_info" | jq -r '.tx[]')
+
+for txid in $block_txs; do
+
+tx_details=$(bitcoin-cli getrawtransaction $txid)
+
+decoded_tx=$(bitcoin-cli decoderawtransaction $tx_details)
+
+output_quantity=$(echo "$decoded_tx" | jq '.vout | length')
+
+total_outputs=$((total_outputs + output_quantity))
+
+done
+
+echo "$total_outputs"
