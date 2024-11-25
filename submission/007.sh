dc656ed36c9362425d411c09ac4d03d96f3327f0
thgOyo <thiagooyo@gmail.com>
2024-11-24 21:07:06 -0300

    007 and 008

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,24 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+
+
+block_hash=$(bitcoin-cli  getblockhash 123321)
+
+block_txs=$(bitcoin-cli  getblock $block_hash | jq -r '.tx[]')
+
+for txid in $block_txs; do
+    vout_index=0  
+    while true; do
+        unspent_tx=$(bitcoin-cli gettxout "$txid" "$vout_index" 2>/dev/null)
+
+        if [ -z "$unspent_tx" ]; then
+            break
+        fi
+
+        unspent_tx_address=$(echo "$unspent_tx" | jq -r '.scriptPubKey.address')
+        if [ "$unspent_tx_address" != "null" ]; then
+            echo "$unspent_tx_address"
+        fi
+
+        vout_index=$((vout_index + 1))
+    done
+done
--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,16 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+
+
+txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+
+raw_tx=$(bitcoin-cli getrawtransaction $txid)
+
+witness_script=$(bitcoin-cli decoderawtransaction $raw_tx | jq -r '.vin[0].txinwitness[2]')
+
+decoded_script=$(bitcoin-cli decodescript $witness_script | jq -r '.asm')
+
+key=$(echo "$decoded_script" | awk '{print $2}')
+
+echo "$key"
+    
